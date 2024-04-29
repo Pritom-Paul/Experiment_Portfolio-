b8f2914b6eba2cacfd592f326f23bf999ff56546
Pritom-Paul <p.paulprem@gmail.com>
2024-04-30 01:13:52 +0600

    About

--- src/Components/About.js
+++ src/Components/About.js
@@ -14,7 +14,7 @@ const About = () => {
               Hi, my name is <b>Pritom Paul</b> and I am from Dhaka, Bangladesh.
               I'm a <b>software developer</b> and I graduated with a degree in <b>Computer Science and Engineering</b> from Brac University in Summer 2023. <br/><br/>
               I thrive on diving deep into the realms of code, constantly expanding my skill set while exploring new horizons. My interests even extend to the fascinating world of <b>Robotic Process Automation (RPA)</b>, where I've found innovative ways to optimize processes and boost efficiency.<br/><br/>
-              Passionate about making a meaningful impact in the tech industry, I enjoy the challenge of pushing boundaries and crafting solutions that truly make a difference.<br/><br/>
+              Passionate about making a meaningful impact in the <b>tech industry</b>, I enjoy the challenge of pushing boundaries and crafting solutions that truly make a difference.<br/><br/>
               Every day presents an opportunity for growth and learning, and I'm always excited about the next big challenge that awaits.<br/>
               I am <b>open</b> to new collaborations or work opportunities where I can contribute and continue to grow. Feel free to connect with me!
           </p>
